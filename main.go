cdd3ef610ab14e4d5333f9459b222c32550fb7fc
Agustin Mansilla <mansillaagustin1019@gmail.com>
2023-05-13 18:44:51 -0300

    Enter name and number of tickets

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	const conferenceTickets = 50
+	conferenceName := "Go Conference"
+	remainingTickets := 50
+
+	fmt.Println("Welcome to",conferenceName,"booking application")
+	fmt.Println("We have total of ",conferenceTickets, "tickets and", remainingTickets,"are stil available")
+	fmt.Println("Get your tickets here to attend!")
+
+	var userName string
+	var numberTickets int 
+	fmt.Println("Enter your name:")
+	fmt.Scan(&userName)
+	fmt.Println("Enter the numbers of tickets that you want to: ")
+	fmt.Scan(&numberTickets)
+
+
+	fmt.Printf("%v booked %v tickets\n",userName,numberTickets)
+
+
 }
